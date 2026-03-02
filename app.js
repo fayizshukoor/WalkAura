777216a2d1ba99b4c8ebe662445994de67ce7fea
fayizshukoor <fayizshukoorkk@gmail.com>
2026-03-03 01:44:03 +0530

    bug fixing in signup

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "development",
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
     },
